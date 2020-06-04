2fea3e127fd00e4ba02b0341a209476b34f0c6b9
woodyfine <1093520060@qq.com>
2020-06-04 16:47:35 +0800

    add transactional

--- dyj/mk_boot_scala/src/main/scala/com/dyj/service/MetaTableService.scala
+++ dyj/mk_boot_scala/src/main/scala/com/dyj/service/MetaTableService.scala
@@ -4,10 +4,12 @@ import com.dyj.domain.MetaTable
 import com.dyj.repository.MetaTableRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class MetaTableService @Autowired()(metaTableRepository: MetaTableRepository) {
 
+  @Transactional
   def save(metaTable:MetaTable) = {
     metaTableRepository.save(metaTable)
   }
